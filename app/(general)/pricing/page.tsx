dc66fd3d43948cfa1516bd87a0ec6666f94f8646
unknown <eviespi@gmail.com>
2023-12-25 23:16:45 -0500

    first commit

---
+++ app/(general)/pricing/page.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata= {
+    title: 'Pracing Page',
+    description: 'Pracing Page'
+}
+
+export default function PrincingPage() {
+    return (
+        <>
+            <span>Pricing Page</span>
+        </>
+    )
+}
